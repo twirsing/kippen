a081f356c11016320037b27f2d34bb1212a63999
matthias.farwick <matthias.farwick@uibk.ac.at>
2014-10-13 16:21:42 +0200

    Visuals text

--- KippenVisuals/src/Visuals.java
+++ KippenVisuals/src/Visuals.java
@@ -12,6 +12,8 @@ import processing.data.JSONObject;
 public class Visuals extends PApplet {
 	public CubeObject[] objects = new CubeObject[4];
 
+	private int session = 1;
+
 	// static ServerSocket variable
 	private static ServerSocket server;
 	// socket server port on which it will listen
@@ -19,24 +21,25 @@ public class Visuals extends PApplet {
 
 	public void setup() {
 		size(displayWidth, displayHeight);
-		
+
 		frameRate(20);
 		background(0);
-		text("UM-KIPPEN", (width / 2) - 40, 40);
+		text("UM-KIPPEN V1.0 Session#" + this.session, (width / 2) - 80, 40);
 
-		objects[0] = new CubeObject(this, random(width), random(height), color(255, 204, 0), color(255, 100, 50, 15));
-		objects[1] = new CubeObject(this, random(width), random(height), color(50, 55, 100), color(255, 120, 0, 15));
-		objects[2] = new CubeObject(this, random(width), random(height), color(34, 102, 102), color(134, 122, 102, 15));
-		objects[3] = new CubeObject(this, random(width), random(height), color(34, 102, 102), color(224, 12, 2, 15));
+		objects[0] = new CubeObject(this, random(width), random(height), color(255, 204, 0), color(255, 100, 200, 15));
+		objects[1] = new CubeObject(this, random(width), random(height), color(50, 200, 100), color(255, 250, 0, 15));
+		objects[2] = new CubeObject(this, random(width), random(height), color(200, 102, 102), color(134, 122, 102, 15));
+		objects[3] = new CubeObject(this, random(width), random(height), color(100, 250, 200), color(10, 200, 2, 15));
 
 		new Thread(new MessageServer(this)).start();
 	}
-	
-	private void reset(){
-		 Date date = Calendar.getInstance().getTime();
-		saveFrame("kippen-" + date.getHours() + ":" + date.getMinutes()  + ":" + date.getSeconds() + ".png");
+
+	private void reset() {
+		Date date = Calendar.getInstance().getTime();
+		saveFrame("kippen-" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ".png");
 		background(0);
-		text("UM-KIPPEN", (width / 2) - 40, 40);
+		session++;
+		text("UM-KIPPEN V1.0: Session#" + this.session, (width / 2) - 40, 40);
 	}
 
 	private class MessageServer implements Runnable {
@@ -67,23 +70,24 @@ public class Visuals extends PApplet {
 							int clipNumber = jsonObject.getInt("clipNumber");
 							System.out.println(cubeNumber);
 							canvas.objects[cubeNumber].start();
-							canvas.objects[cubeNumber].sideChange();
+							canvas.objects[cubeNumber].sideChange(cubeNumber);
 						}
 
 						else if (jsonObject.getString("command").equals("stop")) {
 							int cubeNumber = jsonObject.getInt("trackNumber");
 							canvas.objects[cubeNumber].stop();
-							
+
 							boolean oneRunning = false;
-							//check if all are stopped
-							for(CubeObject o : objects){
-								if(o.isRunning()){
+							// check if all are stopped
+							for (CubeObject o : objects) {
+							//	if (o.isRunning() && !o.getCurrentSide() == 0) {
+								if (o.isRunning() ) {
 									oneRunning = true;
 									break;
 								}
 							}
-							
-							if(!oneRunning)
+
+							if (!oneRunning)
 								reset();
 						}
 
@@ -91,8 +95,6 @@ public class Visuals extends PApplet {
 							int cubeNumber = jsonObject.getInt("trackNumber");
 							Double value = Double.valueOf(jsonObject.getString("value"));
 							int valueInt = (int) (value * 250);
-							System.out.println("value int " + valueInt);
-							System.out.println("bareel "  + value);
 							canvas.objects[cubeNumber].changeColor(valueInt);
 						}
 					}
@@ -127,6 +129,12 @@ public class Visuals extends PApplet {
 
 class CubeObject {
 	private boolean isRunning = true;
+	private int currentSide = 0;
+
+	public int getCurrentSide() {
+		return currentSide;
+	}
+
 
 	PApplet canvas;
 	private int stroke;
@@ -157,10 +165,10 @@ class CubeObject {
 		wander_offset = canvas.random(-max_wander_offset, max_wander_offset);
 	}
 
-	public boolean isRunning(){
+	public boolean isRunning() {
 		return isRunning;
 	}
-	
+
 	void stayInsideCanvas() {
 		x %= canvas.width;
 		y %= canvas.height;
@@ -170,14 +178,14 @@ class CubeObject {
 		this.isRunning = false;
 	}
 
-	public void sideChange() {
+	public void sideChange(int cubeNumber) {
 		wander_offset = canvas.random(-max_wander_offset, max_wander_offset);
 	}
 
-	public void changeColor(int color){
-		this.stroke = canvas.color(255, color, color/2, 15);
+	public void changeColor(int color) {
+		this.stroke = canvas.color(255, color, color / 2, 15);
 	}
-	
+
 	public void toggleIsRunning() {
 		System.out.println("toggle");
 		if (isRunning) {
